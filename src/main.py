a7104536bc8cde5df1ea291cd7ae898f2f2f2971
merlindk <merlinsn@gmail.com>
2025-06-12 20:54:17 +0200

    adding build pipeline

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import time
 import platform
 import joblib
@@ -21,14 +22,7 @@ logging.basicConfig(
 )
 logger=logging.getLogger("adult-income")
 
-run_name = f"run-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
-
-# MLflow config
-MLFLOW_URI = "http://20.237.86.247:5000/"
-EXPERIMENT_NAME = "adult-income-merlin-nunez"
-
-mlflow.set_tracking_uri(MLFLOW_URI)
-mlflow.set_experiment(EXPERIMENT_NAME)
+run_name = os.getenv('RUN_NAME', 'run_name not found')
 
 # Paths
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
@@ -37,6 +31,8 @@ MODEL_DIR = PROJECT_ROOT / "models"
 MODEL_DIR.mkdir(exist_ok=True)
 
 def main():
+    mlflow.set_tracking_uri(os.getenv('MLFLOW_URL', 'http://localhost:5000'))
+    mlflow.set_experiment(os.getenv('EXPERIMENT_NAME', 'experiment_name_not_found'))
     script_start = time.time()
     logger.info(f"System info: {platform.platform()}")
 
